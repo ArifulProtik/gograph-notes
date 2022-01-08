8c30b06e191cc428886732d6e4664ef8e02141eb
ArifulProtik <mdarifulislam.protik@gmail.com>
2022-01-08 16:43:47 +0600

    🎨 adding Config and Log Package

---
+++ log/logger.go
@@ -0,0 +1,142 @@
+/*
+	A log Package Using "go.uber.org/zap"
+*/
+package log
+
+import (
+	"os"
+
+	"github.com/ArifulProtik/gograph-notes/config"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+// Logger methods interface
+type Logger interface {
+	InitLogger()
+	Debug(args ...interface{})
+	Debugf(template string, args ...interface{})
+	Info(args ...interface{})
+	Infof(template string, args ...interface{})
+	Warn(args ...interface{})
+	Warnf(template string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(template string, args ...interface{})
+	DPanic(args ...interface{})
+	DPanicf(template string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(template string, args ...interface{})
+}
+
+// Logger
+type apiLogger struct {
+	cfg         *config.Config
+	sugarLogger *zap.SugaredLogger
+}
+
+// App Logger constructor
+func NewApiLogger(cfg *config.Config) *apiLogger {
+	return &apiLogger{cfg: cfg}
+}
+
+// For mapping config logger to app logger levels
+var loggerLevelMap = map[string]zapcore.Level{
+	"debug":  zapcore.DebugLevel,
+	"info":   zapcore.InfoLevel,
+	"warn":   zapcore.WarnLevel,
+	"error":  zapcore.ErrorLevel,
+	"dpanic": zapcore.DPanicLevel,
+	"panic":  zapcore.PanicLevel,
+	"fatal":  zapcore.FatalLevel,
+}
+
+// Init logger
+func (l *apiLogger) InitLogger() {
+	level := loggerLevelMap["debug"]
+	logWriter := zapcore.AddSync(os.Stdout)
+
+	var encoderCfg zapcore.EncoderConfig
+	if l.cfg.Status == "Dev" {
+		encoderCfg = zap.NewDevelopmentEncoderConfig()
+	} else {
+		encoderCfg = zap.NewProductionEncoderConfig()
+	}
+
+	var encoder zapcore.Encoder
+	encoderCfg.LevelKey = "LEVEL"
+	encoderCfg.CallerKey = "CALLER"
+	encoderCfg.TimeKey = "TIME"
+	encoderCfg.NameKey = "NAME"
+	encoderCfg.MessageKey = "MESSAGE"
+
+	encoder = zapcore.NewConsoleEncoder(encoderCfg)
+
+	encoderCfg.EncodeTime = zapcore.ISO8601TimeEncoder
+	core := zapcore.NewCore(encoder, logWriter, zap.NewAtomicLevelAt(level)) // generic log level
+	logger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(1))
+
+	l.sugarLogger = logger.Sugar()
+	//TODO: Add a Custom Log Writer to a File Using zap - ArifulProtik
+	// zap loger sync doesnt work on Linux Ignoring Argument Error
+	//if err := l.sugarLogger.Sync(); err != nil {
+	//	l.sugarLogger.Error(err)
+	//}
+}
+
+// Logger methods
+
+func (l *apiLogger) Debug(args ...interface{}) {
+	l.sugarLogger.Debug(args...)
+}
+
+func (l *apiLogger) Debugf(template string, args ...interface{}) {
+	l.sugarLogger.Debugf(template, args...)
+}
+
+func (l *apiLogger) Info(args ...interface{}) {
+	l.sugarLogger.Info(args...)
+}
+
+func (l *apiLogger) Infof(template string, args ...interface{}) {
+	l.sugarLogger.Infof(template, args...)
+}
+
+func (l *apiLogger) Warn(args ...interface{}) {
+	l.sugarLogger.Warn(args...)
+}
+
+func (l *apiLogger) Warnf(template string, args ...interface{}) {
+	l.sugarLogger.Warnf(template, args...)
+}
+
+func (l *apiLogger) Error(args ...interface{}) {
+	l.sugarLogger.Error(args...)
+}
+
+func (l *apiLogger) Errorf(template string, args ...interface{}) {
+	l.sugarLogger.Errorf(template, args...)
+}
+
+func (l *apiLogger) DPanic(args ...interface{}) {
+	l.sugarLogger.DPanic(args...)
+}
+
+func (l *apiLogger) DPanicf(template string, args ...interface{}) {
+	l.sugarLogger.DPanicf(template, args...)
+}
+
+func (l *apiLogger) Panic(args ...interface{}) {
+	l.sugarLogger.Panic(args...)
+}
+
+func (l *apiLogger) Panicf(template string, args ...interface{}) {
+	l.sugarLogger.Panicf(template, args...)
+}
+
+func (l *apiLogger) Fatal(args ...interface{}) {
+	l.sugarLogger.Fatal(args...)
+}
+
+func (l *apiLogger) Fatalf(template string, args ...interface{}) {
+	l.sugarLogger.Fatalf(template, args...)
+}
