f42f95c4ee4db075cec9cb20e188cd23d8453fcc
ArifulProtik <mdarifulislam.protik@gmail.com>
2022-01-08 16:42:54 +0600

    🎨 Adding Config and Log Package

---
+++ config/config.go
@@ -0,0 +1,42 @@
+/*
+Config Package Serves All Necessary Configuration Variable With Func LoadConfig Using Go Viper.
+
+*/
+
+package config
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	ServerConfig `mapstructure:",squash"`
+}
+type ServerConfig struct {
+	App     string `mapstructure:"APP"`
+	Version string `mapstructure:"VERSION"`
+	Status  string `mapstructure:"STATUS"`
+	Port    string `mapstructure:"PORT"`
+}
+type Postegres struct {
+	DB_URL string
+}
+
+// LoadConfig maps all enviornment variable to Config Struct Using gomapstructure https://github.com/mitchellh/mapstructure
+func LoadConfig(path string, filename string, configtype string) (*Config, error) {
+	var c Config
+	v := viper.New()
+	v.AddConfigPath(path)
+	v.SetConfigName(filename)
+	v.SetConfigType(configtype)
+	v.AutomaticEnv()
+
+	err := v.ReadInConfig()
+	if err != nil {
+		return nil, err
+	}
+	err = v.Unmarshal(&c)
+	if err != nil {
+		return nil, err
+	}
+	return &c, nil
+
+}
